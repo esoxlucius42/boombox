23eb027097a2780d4cdbf733a8092f89943a1595
Esox Lucius <esoxlucius4@gmail.com>
2026-05-24 18:54:50 +0200

    initial release

--- build/CMakeFiles/boombox.dir/boombox_autogen/3YJK5W5UP7/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/boombox.dir/boombox_autogen/3YJK5W5UP7/qrc_resources.cpp.o: \
- /var/home/esox/dev/cpp/boombox/build/boombox_autogen/3YJK5W5UP7/qrc_resources.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/test_logger_exe.dir/test_logger_exe_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/test_logger_exe.dir/test_logger_exe_autogen/mocs_compilation.cpp.o: \
- /var/home/esox/dev/cpp/boombox/build/test_logger_exe_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
